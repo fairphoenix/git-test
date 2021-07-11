96121fcebd560449bff028bfc956195bd6e163a9
anatoliy <anatoliy.shkabara@gmail.com>
2021-07-11 17:54:30 +0300

    feature 2 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello GIT");
         System.out.println("Feature");
+        System.out.println("feature 2");
     }
 }
